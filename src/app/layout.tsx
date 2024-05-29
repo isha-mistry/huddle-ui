368641cf9eddf603e50008e6c1b4a391cdd7ed28
isha-mistry <ishamistry.lamprostech@gmail.com>
2024-05-29 11:35:25 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import HuddleContextProvider from "@/context/HuddleContextProvider";
+import { Web3Modal } from "@/context/Web3Modal";
+
+export const metadata: Metadata = {
+  title: "Health01",
+  description: "Studio to record podcasts and videos",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Web3Modal>
+          <HuddleContextProvider>{children}</HuddleContextProvider>
+        </Web3Modal>
+      </body>
+    </html>
+  );
+}
